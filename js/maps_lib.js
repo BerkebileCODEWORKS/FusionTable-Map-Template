3b7e9796c1127ddc57ecca95e380fab3dc2dadcc
BerkebileCODEWORKS <marketing@berkebileoil.com>
2017-06-20 09:23:18 -0400

    Update maps_lib.js

--- js/maps_lib.js
+++ js/maps_lib.js
@@ -122,8 +122,8 @@
                     if (self.searchRadius >= 1610000) map.setZoom(4); // 1,000 miles
                     else if (self.searchRadius >= 805000) map.setZoom(5); // 500 miles
                     else if (self.searchRadius >= 402500) map.setZoom(6); // 250 mile
-                    else if (self.searchRadius >= 160934) map.setZoom(7); // 100 miles
-                    else if (self.searchRadius >= 80467) map.setZoom(8); // 50 miles
+                    else if (self.searchRadius >= 160934) map.setZoom(8); // 100 miles
+                    else if (self.searchRadius >= 80467) map.setZoom(9); // 50 miles
                     else if (self.searchRadius >= 16094) map.setZoom(11); // 10 miles
                     else if (self.searchRadius >= 8047) map.setZoom(12); // 5 miles
                     else if (self.searchRadius >= 3220) map.setZoom(13); // 2 miles
